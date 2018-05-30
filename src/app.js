a40708b25bbc486c4437aa014a8c90a79e997186
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-05-30 18:31:30 +0800

    Initial redux setup

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Container, Content } from 'native-base'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import reducers from './reducers'
 
 import { HeaderComponent } from './components/common'
 
 const App = () => {
 	return (
-		<Container>
-			<Content>
-				<HeaderComponent title="Tech Stack" />
-			</Content>
-		</Container>
+		<Provider store={createStore(reducers)}>
+			<Container>
+				<Content>
+					<HeaderComponent title="Tech Stack" />
+				</Content>
+			</Container>
+		</Provider>
 	)
 }
 
